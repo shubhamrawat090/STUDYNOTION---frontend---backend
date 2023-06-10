fbadf073715e70e3a80fc6a50f65e566c8e2e772
Grudge69 <shubham.shubham.rawat090@gmail.com>
2023-06-10 21:43:21 +0530

    refactor

---
+++ StudyNotion/src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCourseCategories } from '../../../../../services/apiconnector'
+
+const CourseInformationForm = () => {
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        getValues,
+        formState: { errors },
+    } = useForm();
+
+    const dispatch = useDispatch();
+    const { course, editCourse } = useSelector(state => state.course);
+    const [loading, setLoading] = useState(false);
+    const [courseCategories, setCourseCategories] = useState([]);
+
+    useEffect(() => {
+        const getCategories = async () => {
+            setLoading(true);
+            const categories = await fetchCourseCategories();
+            if (categories.length > 0) {
+                setCourseCategories(categories);
+            }
+            setLoading(false);
+        }
+
+        if (editCourse) {
+            setValue("courseTitle", course.courseName);
+            setValue("courseShortDesc", course.courseDescription);
+            setValue("coursePrice", course.price);
+            setValue("courseTags", course.tag);
+            setValue("courseBenefits", course.whatYouWillLearn);
+            setValue("courseCategory", course.category);
+            setValue("courseRequirements", course.instructions);
+            setValue("courseImage", course.thumbnail);
+        }
+
+        getCategories();
+    })
+
+    const onSubmit = async () => {
+
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CourseInformationForm
---
+++ StudyNotion/src/components/core/Dashboard/AddCourse/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import RenderSteps from './RenderSteps'
+
+const index = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <h1>Add Course</h1>
+                    <div>
+                        <RenderSteps />
+                    </div>
+                </div>
+
+                <div>
+                    <p>Code Upload Tips</p>
+                    <ul>
+                        <li>copy from figma file</li>
+                        <li>copy from figma file</li>
+                        <li>copy from figma file</li>
+                        <li>copy from figma file</li>
+                        <li>copy from figma file</li>
+                        <li>copy from figma file</li>
+                        <li>copy from figma file</li>
+                        <li>copy from figma file</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default index
